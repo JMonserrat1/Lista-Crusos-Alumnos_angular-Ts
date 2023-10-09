9cd5498d21a3b04295f3ec3889a77483fd63868f
Jere1 <monserratjere@gmail.com>
2023-10-09 13:28:26 -0300

    Proyecto personal practicando angular y typescript

---
+++ src/app/tabla-alumnos/mock.ts
@@ -0,0 +1,29 @@
+import { Alumno } from "../models/Alumno-to"
+
+export const alumnos: Alumno[] = [
+{ 
+    nombre: "Jeremias",
+    apellido: "Monserrat",
+    promedio: 8.5
+},
+{ 
+    nombre: "Albertito",
+    apellido: "Ramos",
+    promedio: 7.2
+},
+{ 
+    nombre: "Roberto",
+    apellido: "Filipino",
+    promedio: 8
+},
+{ 
+    nombre: "Camila",
+    apellido: "Alvarez",
+    promedio: 10
+},
+{ 
+    nombre: "Martin",
+    apellido: "Jimenez",
+    promedio: 5.4
+},
+]
---
+++ src/app/tabla-cursos/mock.ts
@@ -0,0 +1,29 @@
+import { Curso } from "../models/Curso-to"
+
+export const cursos: Curso[] = [
+    { 
+        materia: "Matematicas",
+        nota: 9,
+        debe: "final"
+    },
+    { 
+        materia: "Inlges",
+        nota: 9,
+        debe: "Coloqueo"
+    },
+    { 
+        materia: "Administracion",
+        nota: 10,
+        debe: "Final"
+    },
+    { 
+        materia: "Ingenieria de Software",
+        nota: 10,
+        debe: "Nada"
+    },
+    { 
+        materia: "Comunicacion",
+        nota: 5,
+        debe: "Final"
+    },
+    ]
